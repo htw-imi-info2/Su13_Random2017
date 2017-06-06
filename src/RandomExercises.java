21c2808c2965bbe4e035cd9915acfb4fe7ca7f76
B Kleinen <kleinen@infrastructure.de>
2017-06-06 15:06:06 +0200

    initial commit

---
+++ src/RandomExercises.java
@@ -0,0 +1,26 @@
+
+public class RandomExercises {
+	//double x = Math.random();
+	static int randomBetween0AndN(int n){
+		return -1;
+	}
+	static int randomBetweenLowAndHigh(int low, int high){
+		return -1;
+	}
+	static  int randomInt(int low, int high){
+		return -1;
+	}
+	
+	public static void main(String[] a){
+		
+		int x=1;
+		for(int i=1; i<x; i++)
+			System.out.println("randomBetween0AndN: "+randomBetween0AndN(6));
+		for(int i=1; i<x; i++)
+			System.out.println("randomBetweenLowAndHigh: "+randomBetweenLowAndHigh(1,6));
+		for(int i=1; i<x; i++)
+			System.out.println("randomInt: "+randomInt(1,6));
+			
+		
+	}
+}
