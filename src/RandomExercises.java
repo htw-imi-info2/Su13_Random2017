778316ddc623a4f67ff586c66baf2f1e548c24bc
B Kleinen <kleinen@infrastructure.de>
2017-06-06 22:36:41 +0200

    solution random exercises in class 6/6/17

--- src/RandomExercises.java
+++ src/RandomExercises.java
@@ -1,24 +1,28 @@
 
 public class RandomExercises {
-	//double x = Math.random();
+	
+	
 	static int randomBetween0AndN(int n){
-		return -1;
+		double x = Math.random();
+		return (int)(x*n);
 	}
 	static int randomBetweenLowAndHigh(int low, int high){
-		return -1;
+		double x = Math.random();
+		return (int)(x*(high-low)) +low;
 	}
 	static  int randomInt(int low, int high){
-		return -1;
+		double x = Math.random();
+		return (int)(x*(high-low+1)) +low;
 	}
 	
 	public static void main(String[] a){
 		
-		int x=1;
-		for(int i=1; i<x; i++)
-			System.out.println("randomBetween0AndN: "+randomBetween0AndN(6));
-		for(int i=1; i<x; i++)
+		int x=10;
+		for(int i=0; i<x; i++)
+			System.out.println("randomBetween0AndN: "+randomBetween0AndN(4));
+		for(int i=0; i<x; i++)
 			System.out.println("randomBetweenLowAndHigh: "+randomBetweenLowAndHigh(1,6));
-		for(int i=1; i<x; i++)
+		for(int i=0; i<x; i++)
 			System.out.println("randomInt: "+randomInt(1,6));
 			
 		
